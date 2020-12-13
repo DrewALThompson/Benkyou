8ec31a8b81a49784a745b1a7b36c30eef709037a
Drew Thompson <drewallynthompson@gmail.com>
2020-12-13 01:33:03 -0700

    AppCon helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :require_login, :require_admin
+
+    def current_user
+        session[:user_id] ||= nil
+        if session[:user_id] != nil
+            @user = User.find_by(id: params[:user_id])
+        end
+    end
+
+    def require_login
+        redirect_to root_url unless current_user
+    end
+
+    def require_admin
+        redirect_to root_url unless current_user.admin
+    end 
+
+
 end
