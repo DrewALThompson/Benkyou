6aca2a22d6c35dc9d8da9f7ec6bc5e6b4ab0fc0c
Drew Thompson <drewallynthompson@gmail.com>
2020-12-15 01:50:17 -0700

    User Basics 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end 
+
+    def new
+        
+    end 
+
+    def create 
+    end 
+
+    def show
+    end 
+
+    def edit 
+    end
+
+    def update
+    end 
+
+    def delete
+    end 
+
+    private
+
+    def find_user
+        @user = User.find_by(id: params[:id])
+    end 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :words
     has_many :comments, through: :projects
     has_many :comments, through: :translations
+    has_secure_password
 end
