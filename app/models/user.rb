57ae36fd767befdafea567a00559d1d9bcf8d024
Drew Thompson <drewallynthompson@gmail.com>
2020-12-16 00:36:05 -0700

    User model and index adjustment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,14 @@ class User < ApplicationRecord
     has_many :comments, through: :translations
     has_secure_password
 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
     validates :name, presence: true,
                length: { minimum: 3, maximum: 254},
-               format: { with: => /[一-龯ぁ-ゔゞァ-・ヽヾ゛゜ーa-zA-Z]/ },
+               format: { with: /\A[一-龯ぁ-ゔゞァ-・ヽヾ゛゜ーa-zA-Z]/ },
                uniqueness: true
     validates :email, presence: true,
-            format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
+            format: { with: VALID_EMAIL_REGEX, message: "Email invalid" },
             uniqueness: { case_sensitive: false },
             length: { minimum: 4, maximum: 254 }   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_112456) do
+ActiveRecord::Schema.define(version: 2020_12_14_070952) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
-    t.integer "project_id"
     t.integer "translation_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_id"], name: "index_comments_on_project_id"
     t.index ["translation_id"], name: "index_comments_on_translation_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.integer "translation_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["translation_id"], name: "index_projects_on_translation_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
+  create_table "translations", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
