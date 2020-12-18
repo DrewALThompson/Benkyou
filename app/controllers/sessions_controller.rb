6e4d698bd98023da8694cd14a026ab6dee9c6aa0
Drew Thompson <drewallynthompson@gmail.com>
2020-12-18 03:13:16 -0700

    Sessions work notes in scss files

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
     end 
 
     def create
+        user = User.find_by(name: params[:user][:name])
+        if user && user.authenticate(password: params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to root_url
+        else
+            flash[:notice] = 'Username or Password was incorrect'
+            render :new
+        end
     end 
 
     def destroy
