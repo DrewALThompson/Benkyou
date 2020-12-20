ebf8a8e10b0018e90070f4e8dda1439d90a247ab
Drew Thompson <drewallynthompson@gmail.com>
2020-12-20 01:21:37 -0700

    Project index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,35 @@
 class ProjectsController < ApplicationController
+    before_action :require_login, except: [:index, :show]
+
     def index
         @projects = Project.all
+    end
+
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.create(title: params[:title], content: params[:content], owner_id: params[:owner_ids])
+        if @project.valid?
+            redirect_to project_path(@project)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+
+    def find_project
+        @project = Project.find(params[:id])
     end
 end
