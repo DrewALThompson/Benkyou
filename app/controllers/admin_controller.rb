396db8bd0a683a745782e1b88b97ed6c3ab6e86e
Drew Thompson <drewallynthompson@gmail.com>
2020-12-23 01:29:26 -0700

    Admin tweak

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,12 @@
 class AdminController < ApplicationController
+    before_action :require_admin, only: [:index, :edit, :update, :stats]
     def index 
+        @users = User.all
+        @projects = Project.all
     end 
 
     def edit
+
     end
 
     def update
