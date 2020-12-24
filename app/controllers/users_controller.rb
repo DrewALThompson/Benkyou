e728c95fdf33718b3db47314fcb163e6b1f25643
Drew Thompson <drewallynthompson@gmail.com>
2020-12-24 00:22:23 -0700

    Requirement edits users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :find_user, only: [:show, :edit, :update, :delete]
+    before_action :require_login, only: [:edit, :update, :delete]
+    before_action :require_admin, only: [:index]
 
     def index
         @users = User.all
